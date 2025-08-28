9d898e33fa59a3e1a1cc76b92c5053f2d60578fe
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-29 02:10:16 +0600

    Template string, multiple line string, dynamic string

---
+++ template.js
@@ -0,0 +1,27 @@
+const first = 'jaan'
+const last = 'pakhi'
+const greet = 'potas potas'
+const name = first+ ' ' + last + ' '+ greet;
+console.log(name);
+
+const a = 10;
+const b = 20;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+
+const numbers = [45, 48, 98, 78];
+const student = {name:'john ochena', age: 5}
+// const math =`The sum of ${numbers[0]} and ${b} is ${a + b}`;
+const math =`The sum of ${numbers[0]} and ${student.age} is ${numbers[0] + student.age}`;
+console.log(math);
+
+const email = 'hi john \n' +
+'chena chena lage\n' +
+'tumi mara mari korba naki'
+;
+console.log(email);
+
+const challange = `john cena
+chena naki ochena
+mair dimu prochur`;
+console.log(challange);
